37ba6f35fa27878e7eaf05d6f46d279d2902b878
timothee-dhenain-zenika <timothee.dhenain@zenika.com>
2023-10-21 14:37:21 -0400

    Resolve missing module in standalone components

--- src/app/features/maximizing-ho-stats/maximizing-ho-stats.component.ts
+++ src/app/features/maximizing-ho-stats/maximizing-ho-stats.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core'
   styleUrls: ['./maximizing-ho-stats.component.scss'],
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
   ]
 })
 export class MaximizingHoStatsComponent {}
--- src/app/features/priority-sublimations/priority-sublimations.component.ts
+++ src/app/features/priority-sublimations/priority-sublimations.component.ts
@@ -1,9 +1,14 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-priority-sublimations',
   templateUrl: './priority-sublimations.component.html',
-  styleUrls: ['./priority-sublimations.component.scss']
+  styleUrls: ['./priority-sublimations.component.scss'],
+  standalone: true,
+  imports: [
+    NgOptimizedImage,
+  ]
 })
 export class PrioritySublimationsComponent {
 
